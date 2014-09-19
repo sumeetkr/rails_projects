8ea881e72ca072819e723cf68373327545fea9f2
Sumeet Kumar <kr.sumeet@gmail.com>
2014-09-18 21:59:00 -0700

    changes beacon id field to string

--- db/migrate/20140917072215_create_scanners.rb
+++ db/migrate/20140917072215_create_scanners.rb
@@ -2,7 +2,7 @@ class CreateScanners < ActiveRecord::Migration
   def change
     create_table :scanners do |t|
       t.string :identifier
-      t.integer :beconId, :limit => 8
+      t.string :beconId, :limit => 8
 
       t.timestamps
     end
